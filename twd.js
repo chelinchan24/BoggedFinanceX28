abbf43b11d5ae3e422183f72c7a044e49d93b516
chelinchan24 <me@chelinchan.com>
2021-07-04 19:51:53 +0800

    更新

--- twd.js
+++ twd.js
@@ -1,10 +1,11 @@
 window.onload = function () {
-    var container = document.createElement('div');
+    var container = document.createElement('h3');
 
     container.setAttribute('id', 'twd-price')
-    container.setAttribute('style', 'font-size:18px !important;  display:block !important; margin-left: 16px; opacity: 0.8')
+    container.setAttribute('class', 'dark:text-primary text-primary-dark mt-2 flex flex-row items-center')
+    container.setAttribute('style', 'font-size:18px !important;  display:block !important; margin: 0px; opacity: 0.8')
 
-    document.querySelector("body > div > div.flex.pt-20.h-full.relative > div > nav > div.p-4 > h3").appendChild(container)
+    document.querySelector("body > div > div.flex.pt-20.h-full.relative > div > nav > div.p-4").appendChild(container)
 
     setInterval(updatePrice, 8000)
 
@@ -12,7 +13,7 @@ window.onload = function () {
         var twd = Number(document.evaluate("/html/body/div/div[1]/div/nav/div[1]/h3/text()", document, null, XPathResult.STRING_TYPE, null).stringValue.split('$')[1])*28
         var numberFormat = new Intl.NumberFormat('en-US')
         var formatedNumber = numberFormat.format(twd.toFixed(3))
-        document.getElementById('twd-price').innerText = 'NT$' + formatedNumber
+        document.getElementById('twd-price').innerText = '≈NT$' + formatedNumber
     }
 
 }
