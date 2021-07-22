be4a8a8b8efdbbe8981f2ccdda42f584ae5966cd
chelinchan24 <me@chelinchan.com>
2021-07-22 13:08:39 +0800

    更新

--- twd.js
+++ twd.js
@@ -1,6 +1,6 @@
 window.onload = function () {
     var wait = setInterval(function() {
-        var balance = document.querySelector("body > div > div.flex.relative.pt-16.h-full > div.flex.fixed.justify-between.border-r.dark\\:border-gray-700.duration-200.overflow-x-hidden.border-gray-200.flex-col.z-40.inset-y-0.left-0.bg-white.dark\\:bg-gray-800.overflow-y-auto.xl\\:static.inset-0.pb-4.w-72 > nav > div:nth-child(4) > div.mt-2.opacity-100 > div > div.px-3.py-2\\.5.dark\\:bg-gray-600.justify-between.flex.flex-row.items-center.dark\\:bg-opacity-40.bg-gray-100.border-b.dark\\:border-gray-700.border-gray-200 > h4 > div")
+        var balance = document.querySelector("body > div > div.flex.relative.pt-16.h-full > div.flex.fixed.justify-between.border-r.dark\\:border-gray-700.duration-200.overflow-x-hidden.border-gray-200.flex-col.z-40.inset-y-0.left-0.bg-white.dark\\:bg-gray-800.overflow-y-auto.xl\\:static.inset-0.pb-4.w-72 > nav > div:nth-child(4) > div.mt-2.opacity-100 > div > div.px-3.py-2\\.5.dark\\:bg-gray-600.justify-between.flex.flex-row.items-center.dark\\:bg-opacity-40.bg-gray-100.border-b.dark\\:border-gray-700.border-gray-200 > h4")
         if (typeof(balance) != 'undefined' && balance != null) {
             loadvalue()
         }
@@ -9,7 +9,7 @@ window.onload = function () {
     function loadvalue() {
         clearInterval(wait);
         var container = document.createElement('p');
-        var balance = document.querySelector("body > div > div.flex.relative.pt-16.h-full > div.flex.fixed.justify-between.border-r.dark\\:border-gray-700.duration-200.overflow-x-hidden.border-gray-200.flex-col.z-40.inset-y-0.left-0.bg-white.dark\\:bg-gray-800.overflow-y-auto.xl\\:static.inset-0.pb-4.w-72 > nav > div:nth-child(4) > div.mt-2.opacity-100 > div > div.px-3.py-2\\.5.dark\\:bg-gray-600.justify-between.flex.flex-row.items-center.dark\\:bg-opacity-40.bg-gray-100.border-b.dark\\:border-gray-700.border-gray-200 > h4 > div")
+        var balance = document.querySelector("body > div > div.flex.relative.pt-16.h-full > div.flex.fixed.justify-between.border-r.dark\\:border-gray-700.duration-200.overflow-x-hidden.border-gray-200.flex-col.z-40.inset-y-0.left-0.bg-white.dark\\:bg-gray-800.overflow-y-auto.xl\\:static.inset-0.pb-4.w-72 > nav > div:nth-child(4) > div.mt-2.opacity-100 > div > div.px-3.py-2\\.5.dark\\:bg-gray-600.justify-between.flex.flex-row.items-center.dark\\:bg-opacity-40.bg-gray-100.border-b.dark\\:border-gray-700.border-gray-200 > h4")
 
         container.setAttribute('id', 'twd-price')
         container.setAttribute('style', 'display: inline-block !important; opacity: 0.8; font-size: 15px;')
